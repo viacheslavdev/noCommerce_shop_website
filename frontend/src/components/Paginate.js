0631a8877a32bfaa23e28ff20ed9b2fbacaf704f
Stryzh V <88396659+viacheslavdev@users.noreply.github.com>
2023-11-07 12:04:34 +0200

    update immutable variables in Paginate.js

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -3,21 +3,21 @@ import {Pagination} from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
 function Paginate({pages, page, keyword='', isAdmin =false }) {
-    console.log(keyword)
 
+    let newKeyword = keyword
+    
     if(keyword){
-        keyword = keyword.split('?keyword=')[1].split('&')[0]
+        newKeyword = keyword.split('?keyword=')[1].split('&')[0]
     }
-
-    console.log(keyword)
+    
     return ( pages > 1 && (
         <Pagination>
             {pages !== undefined && [...Array(pages).keys()].map((x) => (
                 <LinkContainer 
                 key={x+1}
                 to={!isAdmin ? 
-                    { pathname: '/', search: `?keyword=${keyword}&page=${x + 1}` } 
-                    : { pathname: '/admin/productlist/', search: `?keyword=${keyword}&page=${x + 1}` }}
+                    { pathname: '/', search: `?keyword=${newKeyword}&page=${x + 1}` } 
+                    : { pathname: '/admin/productlist/', search: `?keyword=${newKeyword}&page=${x + 1}` }}
                 ><Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
                 </LinkContainer>
             ))}
